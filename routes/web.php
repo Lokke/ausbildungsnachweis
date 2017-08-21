201c58589888b84937587529a65407b9b389f028
chukarave <noa.rave@berlinonline.de>
2017-08-21 14:56:24 +0200

    add routes to form show/post

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,9 @@
 use App\Task;
 use App\Week;
 
-Route::get('/tasks', function () {
-	//Eloquent query
-	$tasks = Task::all();
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-	
-    $task = Task::find($id);
-	
-    return view('tasks.show', compact('task'));
-});
 
 Route::get('/weeks', 'WeeksController@index');
-Route::get('/weeks/{week}', 'WeeksController@show');
+Route::get('/weeks/{week}', 'WeeksController@show')->where('week', '[0-9]{1,3}');
+Route::get('/weeks/add', 'WeeksController@showForm');
+Route::post('/weeks/add', 'WeeksController@postForm');
 
