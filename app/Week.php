48c62a3b8f0bc56e266e0f5d1bf05a1ff8c761a3
chukarave <noa.rave@berlinonline.de>
2017-08-24 14:27:53 +0200

    add new properties to week model

--- app/Week.php
+++ app/Week.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Week extends Model
 {
     protected $guarded = ['id'];
+    protected $week_nr;
+    protected $name;
+    protected $profession;
+    protected $department;
     protected $school;
     protected $work;
     protected $training;
     protected $start_date;
     protected $end_date;
-    protected $fillable = ['school', 'work', 'training', 'start_date', 'end_date'];
+    protected $fillable = ['week_nr', 'name', 'profession', 'department', 'school', 'work', 'training', 'start_date', 'end_date'];
 
 }
