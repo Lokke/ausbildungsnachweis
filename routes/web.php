e12ba4346a09a633a3989be664bb733d2a844d1c
chukarave <noa.rave@berlinonline.de>
2017-08-24 14:26:39 +0200

    add route to export

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ use App\Week;
 Route::get('/weeks', 'WeeksController@index');
 Route::get('/weeks/{week}', 'WeeksController@show')->where('week', '[0-9]{1,3}');
 Route::get('/weeks/add', 'WeeksController@showForm');
+Route::get('/weeks/export/{week}' ,'WeeksController@export')->where('week', '[0-9]{1,3}');;
 Route::post('/weeks/add', 'WeeksController@postForm');
 
