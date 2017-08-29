816a606373669319c2a0f1033daf16f54c43c6df
chukarave <noa.rave@berlinonline.de>
2017-08-29 15:15:28 +0200

    add temporary route to export template

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Week;
 Route::get('/weeks', 'WeeksController@index');
 Route::get('/weeks/{week}', 'WeeksController@show')->where('week', '[0-9]{1,3}');
 Route::get('/weeks/add', 'WeeksController@showForm');
-Route::get('/weeks/export/{week}' ,'WeeksController@export')->where('week', '[0-9]{1,3}');;
+Route::get('/weeks/export/{week}' ,'WeeksController@export')->where('week', '[0-9]{1,3}');
+Route::get('/weeks/exporttemp/{week}' ,'WeeksController@exportTemplate')->where('week', '[0-9]{1,3}');
 Route::post('/weeks/add', 'WeeksController@postForm');
 
